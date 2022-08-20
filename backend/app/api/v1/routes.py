3f9589b81f032a6312e3ae3d75d477d715f025d1
Francisco Barrento <francisco.barrento@gmail.com>
2022-08-21 00:31:53 +0100

    Feature: added extended field to posts

--- backend/app/api/v1/routes.py
+++ backend/app/api/v1/routes.py
@@ -1,7 +1,9 @@
 """Version 1 Base Router"""
+from app.api.v1.posts.routes import router as posts_router
 from app.api.v1.users.routes import router as users_router
 from fastapi import APIRouter
 
-router = APIRouter(prefix="/v1", tags=["v1"])
+router = APIRouter(prefix="/v1")
 
 router.include_router(users_router)
+router.include_router(posts_router)
--- backend/app/models/post.py
+++ backend/app/models/post.py
@@ -1,7 +1,9 @@
 from datetime import datetime
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING, Any, Optional
 
+from app.support.models.mixins import IDMixin
 from sqlalchemy import Column, DateTime
+from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.sql import func
 from sqlmodel import Field, Relationship, SQLModel
 
@@ -11,9 +13,9 @@ if TYPE_CHECKING:
 
 class BasePost(SQLModel):
     __tablename__: str = "posts"
-    id: Optional[int] = Field(default=None, primary_key=True)
     title: str = Field(index=True)
     content: str
+    extended: Optional[dict[str, Any]] = Field(sa_column=Column(JSONB), nullable=True)
     created_at: datetime = Field(
         sa_column=Column(
             DateTime(timezone=True), nullable=False, server_default=func.now()
@@ -32,5 +34,14 @@ class BasePost(SQLModel):
     user: "User" = Relationship(back_populates="posts")
 
 
-class Post(BasePost, table=True):
+class Post(IDMixin, BasePost, table=True):
     pass
+
+
+class PostCreate(SQLModel):
+    title: str
+    content: str
+    extended: Optional[dict]
+    user_id: int
+
+    user: "User" = Relationship(back_populates="posts")
